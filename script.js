29caa7934e9966f9161bc8c28d4ee104e89ebd43
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2024-03-18 22:51:23 +0530

    updated

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btn = document.getElementById('btn');
 const toggle1 = document.getElementById('toggle-btn1');
 const toggle2 = document.getElementById('toggle-btn2');
 
+
 const animation = lottie.loadAnimation({
   container: animationContainer,
   // path to your Lottie animation JSON file
@@ -13,14 +14,42 @@ const animation = lottie.loadAnimation({
 
 
 
+window.addEventListener('resize', ()=> {
+  const image1 = document.getElementById('frame439');
+  const image2 = document.getElementById('frame-440');
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    image1.src = "./img/Group.svg";
+    image2.src = "./img/Group (1).svg";
+  } else {
+    image1.src = "./img/Frame 439.svg";
+    image2.src = "./img/Frame 440.svg"
+  }
+});
+
 function rightClick() {
   btn.style.left = 'calc(132px - 2%)';
   toggle2.style.color = '#fff';
-  toggle1.style.color = 'var(--para-color)';
+  setTimeout(toggle1.style.color = 'var(--para-color)', 500)
 }
 
 function leftClick() {
   btn.style.left = '2%';
   toggle1.style.color = '#fff';
-  toggle2.style.color = 'var(--para-color)';
+  setTimeout(toggle2.style.color = 'var(--para-color)', 500)
 }
+
+let activeIndex = 0;
+
+function toggleTestimony(index) {
+  activeIndex = index;
+}
+
+const radioButtons = document.querySelectorAll('input[name="testimony-toggle"]');
+
+radioButtons.forEach((radioButton, index) => {
+  radioButton.addEventListener('change', () => {
+    if (radioButton.checked) {
+      toggleTestimony(index);
+    }
+  });
+});
