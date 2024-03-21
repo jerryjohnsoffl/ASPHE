e276d5c57459a1af079df9dcb0127e84573cce46
adwaithek <adwaithek01@gmail.com>
2024-03-21 11:10:58 +0530

    21-03-24 11:10

--- script.js
+++ script.js
@@ -53,3 +53,35 @@ radioButtons.forEach((radioButton, index) => {
     }
   });
 });
+
+
+// scroller anim
+
+let scrollDirection;
+ 
+
+function addRevealEffect(elements) {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      let revealClass;
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          revealClass = scrollDirection === 'up' ? 'reveal-up' : 'reveal-down';
+
+          entry.target.classList.add(revealClass);
+        } else {
+          entry.target.className = 'subject';
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+}
+
+const elementsToReveal = document.querySelectorAll('.subject');
+addRevealEffect(elementsToReveal);
+ 
